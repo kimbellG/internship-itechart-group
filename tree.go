20a3db3cea381d1dc4af213e8b24c1a0b9af1ecf
artyom <filippenkov95@gmail.com>
2021-07-14 17:31:40 +0300

    comleted

---
+++ tree.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	startDirectory = flag.String("d", ".", "set the start directory")
+)
+
+func main() {
+	flag.Parse()
+
+	p, err := filepath.Abs(*startDirectory)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", p)
+	if err := printDirectoryInformation(p, 0); err != nil {
+		log.Fatalf("tree: %v", err)
+	}
+}
+
+func printDirectoryInformation(dirpath string, hierarchyLevel int) error {
+	files, err := os.ReadDir(dirpath)
+	if err != nil {
+		return fmt.Errorf("reading directory has been failed: %v", err)
+	}
+
+	for _, file := range files {
+		fmt.Printf("%v---/%v\n", strings.Repeat("----", hierarchyLevel), file.Name())
+		if file.IsDir() {
+			if err := printDirectoryInformation(filepath.Join(dirpath, file.Name()), hierarchyLevel+1); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
