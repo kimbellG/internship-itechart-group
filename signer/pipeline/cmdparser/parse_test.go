a85b0c55b69848afaf9e1e76a933c39195c4ea59
artyom <filippenkov95@gmail.com>
2021-07-21 12:10:02 +0300

    added test for parsing all pipeline input
    signer

--- signer/pipeline/cmdparser/parse_test.go
+++ signer/pipeline/cmdparser/parse_test.go
@@ -42,3 +42,47 @@ func TestParseOneCmd(t *testing.T) {
 		}
 	}
 }
+
+func TestAllCmd(t *testing.T) {
+	testInfos := []struct {
+		input string
+		want  []cmdInformation
+	}{
+		{
+			"sort -c",
+			[]cmdInformation{{"sort", []string{"-c"}, ""}},
+		},
+		{
+			"sort -c | awk abde | test -a -s",
+			[]cmdInformation{{"sort", []string{"-c"}, "|"}, {"awk", []string{"abde"}, "|"}, {"test", []string{"-a", "-s"}, ""}},
+		},
+	}
+
+	for _, testInfo := range testInfos {
+		result, err := Parse(testInfo.input)
+		if err != nil {
+			t.Errorf("error from func: %v", err)
+			continue
+		}
+
+		if len(testInfo.want) != len(result) {
+			t.Errorf("incorrect len of result. want: %v, result len: %v", len(testInfo.want), len(result))
+			continue
+		}
+
+		for i := range result {
+			if testInfo.want[i].name != result[i].name {
+				t.Errorf("incorrect parsing of input string. name isn't eq. want: %v, result: %v", testInfo.want[i].name, result[i].name)
+			}
+
+			if !reflect.DeepEqual(testInfo.want[i].args, result[i].args) {
+				t.Errorf("incorrect parsing of input string: args aren't eq. want: %v, result: %v", testInfo.want[i].args, result[i].args)
+			}
+
+			if testInfo.want[i].pipelineOperation != result[i].pipelineOperation {
+				t.Errorf("incorrect parsing of input string: operation isn't eq. want: %v, result: %v",
+					testInfo.want[i].pipelineOperation, result[i].pipelineOperation)
+			}
+		}
+	}
+}
