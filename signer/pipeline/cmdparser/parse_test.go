b3f78456c103ef3703d235222b760d97dcb75ce8
artyom <filippenkov95@gmail.com>
2021-07-23 12:54:31 +0300

    added some incorrect way of testing cmd parse function
    added in parse test way for testing a incorrect data input
    
    signer

--- signer/pipeline/cmdparser/parse_test.go
+++ signer/pipeline/cmdparser/parse_test.go
@@ -11,34 +11,47 @@ func TestParseOneCmd(t *testing.T) {
 		inputPipe string
 		wantName  string
 		wantArgs  []string
+		wantErr   string
 	}{
 		{
 			"sort -c test",
 			"|",
 			"sort",
 			[]string{"-c", "test"},
+			"",
 		},
 		{
 			"test",
-			"&",
+			"|",
 			"test",
 			[]string{},
+			"",
+		},
+		{
+			"",
+			"",
+			"",
+			[]string{},
+			"command is not defined",
 		},
 	}
 
 	for _, testInput := range testInputs {
-		result, err := parseCmd(testInput.inputCmd, testInput.inputPipe)
+		got, err := parseCmd(testInput.inputCmd, testInput.inputPipe)
 		if err != nil {
-			t.Errorf("parse command error: %v", err)
+			if err.Error() != testInput.wantErr {
+				t.Errorf("parse command error: %v", err)
+			}
+
 			continue
 		}
 
-		if result.name != testInput.wantName {
-			t.Errorf("name parsing failed: result = %v, want = %v", result.name, testInput.wantName)
+		if got.name != testInput.wantName {
+			t.Errorf("name parsing failed: result = %v, want = %v", got.name, testInput.wantName)
 		}
 
-		if !reflect.DeepEqual(result.args, testInput.wantArgs) {
-			t.Errorf("args parsing failed: result args = %v, want = %v", result.args, testInput.wantArgs)
+		if !reflect.DeepEqual(got.args, testInput.wantArgs) {
+			t.Errorf("args parsing failed: result args = %v, want = %v", got.args, testInput.wantArgs)
 		}
 	}
 }
@@ -47,41 +60,56 @@ func TestAllCmd(t *testing.T) {
 	testInfos := []struct {
 		input string
 		want  []CMDInformation
+		err   string
 	}{
 		{
 			"sort -c",
 			[]CMDInformation{{"sort", []string{"-c"}, ""}},
+			"",
 		},
 		{
 			"sort -c | awk abde | test -a -s",
 			[]CMDInformation{{"sort", []string{"-c"}, "|"}, {"awk", []string{"abde"}, "|"}, {"test", []string{"-a", "-s"}, ""}},
+			"",
+		},
+		{
+			"grep",
+			[]CMDInformation{{"grep", []string{}, ""}},
+			"",
+		},
+		{
+			"",
+			[]CMDInformation{{"", []string{}, ""}},
+			"parse input command: command is not defined",
 		},
 	}
 
 	for _, testInfo := range testInfos {
-		result, err := Parse(testInfo.input)
+		got, err := Parse(testInfo.input)
 		if err != nil {
-			t.Errorf("error from func: %v", err)
+			if err.Error() != testInfo.err {
+				t.Errorf("error from func: %v", err)
+			}
 			continue
 		}
 
-		if len(testInfo.want) != len(result) {
-			t.Errorf("incorrect len of result. want: %v, result len: %v", len(testInfo.want), len(result))
+		if len(testInfo.want) != len(got) {
+			t.Errorf("incorrect len of result. want: %v, got len: %v", len(testInfo.want), len(got))
 			continue
 		}
 
-		for i := range result {
-			if testInfo.want[i].name != result[i].name {
-				t.Errorf("incorrect parsing of input string. name isn't eq. want: %v, result: %v", testInfo.want[i].name, result[i].name)
+		for i := range got {
+			if testInfo.want[i].name != got[i].name {
+				t.Errorf("incorrect parsing of input string. name isn't eq. want: %v, got: %v", testInfo.want[i].name, got[i].name)
 			}
 
-			if !reflect.DeepEqual(testInfo.want[i].args, result[i].args) {
-				t.Errorf("incorrect parsing of input string: args aren't eq. want: %v, result: %v", testInfo.want[i].args, result[i].args)
+			if !reflect.DeepEqual(testInfo.want[i].args, got[i].args) {
+				t.Errorf("incorrect parsing of input string: args aren't eq. want: %v, got: %v", testInfo.want[i].args, got[i].args)
 			}
 
-			if testInfo.want[i].pipelineOperation != result[i].pipelineOperation {
-				t.Errorf("incorrect parsing of input string: operation isn't eq. want: %v, result: %v",
-					testInfo.want[i].pipelineOperation, result[i].pipelineOperation)
+			if testInfo.want[i].pipelineOperation != got[i].pipelineOperation {
+				t.Errorf("incorrect parsing of input string: operation isn't eq. want: %v, got: %v",
+					testInfo.want[i].pipelineOperation, got[i].pipelineOperation)
 			}
 		}
 	}
